b58f84146eb34804740e7742da75179c1bc9fff4
Viktor <vic.lisinski@gmail.com>
2026-06-26 15:29:53 +0200

    fixed

--- src/app/components/pages/ServicePage/ServicePage.tsx
+++ src/app/components/pages/ServicePage/ServicePage.tsx
@@ -1,34 +1,15 @@
 import { FinalCtaContent } from "../../shared/lib/constants";
-import {
-	IntroBlockServicePageContent,
-	RelocationGroundsServicePageContent,
-	TabsContent,
-} from "../../shared/lib/constants/ServicePageContent";
+import { IntroBlockServicePageContent } from "../../shared/lib/constants/ServicePageContent";
 import Tab from "../../shared/Tab";
 import FinalCta from "../../widgets/FinalCta";
 import IntroBlock from "../../widgets/IntroBlock";
-import RelocationGrounds from "./components/RelocationGrounds";
 import styles from "./ServicePage.module.css";
 
-interface PageProps {
-	params: Promise<{ locale: string; slug: string }>;
-}
-
-export default async function ServicePage({ params }: PageProps) {
+export default async function ServicePage() {
 	return (
 		<main className={styles.page}>
 			<IntroBlock {...IntroBlockServicePageContent} />
-			<RelocationGrounds {...RelocationGroundsServicePageContent}>
-				<ul className={styles.tabs}>
-					{TabsContent.tabs.map((item) => (
-						<li
-							key={item.reason}
-							className={styles.tab}>
-							<Tab {...item} />
-						</li>
-					))}
-				</ul>
-			</RelocationGrounds>
+
 			<FinalCta {...FinalCtaContent} />
 		</main>
 	);
--- src/app/components/shared/lib/constants/ServicePageContent.ts
+++ src/app/components/shared/lib/constants/ServicePageContent.ts
@@ -1,12 +1,5 @@
-import { RelocationGroundsProps } from "@/app/components/pages/ServicePage/components/RelocationGrounds/RelocationGrounds.types";
 import { IntroBlockProps } from "@/app/components/widgets/IntroBlock/IntroBlock.types";
-interface TabProps {
-	text: string;
-	reason: string;
-}
-interface TabsProps {
-	tabs: TabProps[];
-}
+
 export const SERVICE_PAGES = ["montenegro", "spain", "hungary"];
 
 export const IntroBlockServicePageContent: IntroBlockProps = {
@@ -16,33 +9,4 @@ export const IntroBlockServicePageContent: IntroBlockProps = {
 	firstButton: {
 		text: "mainPage.intro.firstButton.text",
 	},
-};
-
-export const TabsContent: TabsProps = {
-	tabs: [
-		{
-			text: "Services.Montenegro.requirements.tabMainApplicant",
-			reason: "main-applicant",
-		},
-		{
-			text: "Services.Montenegro.requirements.tabFamilyMembers",
-			reason: "family-members",
-		},
-		{
-			text: "Services.Montenegro.requirements.tabFinances",
-			reason: "finances",
-		},
-		{
-			text: "Services.Montenegro.requirements.tabDocuments",
-			reason: "documents",
-		},
-	],
-};
-
-export const RelocationGroundsServicePageContent: Omit<
-	RelocationGroundsProps,
-	"children"
-> = {
-	eyebrow: "Services.Montenegro.requirements.eyebrow",
-	title: "Services.Montenegro.requirements.title",
 };
