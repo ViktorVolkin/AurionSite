1501626918e2e32480bc133270e9bb95efebac5e
Viktor <vic.lisinski@gmail.com>
2026-07-05 17:43:33 +0200

    suspense

--- src/app/components/pages/BlogPage/BlogPage.tsx
+++ src/app/components/pages/BlogPage/BlogPage.tsx
@@ -3,14 +3,17 @@ import { BLOG_CONTENT } from "../../shared/lib/constants/BlogContent";
 import FinalCta from "../../widgets/FinalCta";
 import BlogCards from "./components/BlogCards";
 import styles from "./BlogPage.module.css";
+import { Suspense } from "react";
 
 export default async function BlogPage() {
 	return (
 		<main>
-			<BlogCards
-				cards={BLOG_CONTENT.cards}
-				title={BLOG_CONTENT.title}
-			/>
+			<Suspense>
+				<BlogCards
+					cards={BLOG_CONTENT.cards}
+					title={BLOG_CONTENT.title}
+				/>
+			</Suspense>
 			<FinalCta {...FinalCtaContent} />
 		</main>
 	);
